cdd4da3131e73494e8e19abf0e86a9423c03821b
rvndudz <rvndudz77@gmail.com>
2024-05-19 13:00:56 +0530

    frontend UI completed

--- frontend/src/components/UserGrid.jsx
+++ frontend/src/components/UserGrid.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { Flex, Grid, Spinner, Text } from "@chakra-ui/react";
+import { USERS } from "../dummy/dummy";
+import UserCard from "./UserCard";
 
 const UserGrid = () => {
-  return <div>UserGrid</div>;
+  return (
+    <>
+      <Grid
+        templateColumns={{
+          base: "1fr",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(3, 1fr)",
+        }}
+        gap={4}
+      >
+        {USERS.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </Grid>
+      (
+      <Flex justifyContent={"center"}>
+        <Spinner size={"xl"} />
+      </Flex>
+      ) (
+      <Flex justifyContent={"center"}>
+        <Text fontSize={"xl"}>
+          <Text as={"span"} fontSize={"2xl"} fontWeight={"bold"} mr={2}>
+            Poor you! 🥺
+          </Text>
+          No friends found.
+        </Text>
+      </Flex>
+      )
+    </>
+  );
 };
-
 export default UserGrid;
